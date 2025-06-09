7ee91aefce38d1e4ba58e60a8dcffccc828469a4
lzephir950 <ludmilla.zephir@gmail.com>
2025-06-09 11:55:16 +0200

    modification page réalisation : ajout projet

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,7 +1,17 @@
 import './projects.css';
-import logoImage from '../image/laravel1.png';
+import logoLaravel from '../image/laravel1.png';
+import logoCss from '../image/css.png';
+import logoHtml from '../image/html.png';
+import logoPhp from '../image/php.png';
+import logoJs from '../image/js.png';
+import logoJava from '../image/java.png';
+import logoJavafx from '../image/javafx.png';
+import logoWordpress from '../image/wordpress.png';
+import logoElementor from '../image/elementor.png';
 import projetImage from '../image/ohmycat.png';
 import biblionest from '../image/biblionest.png';
+import todolist from '../image/todolist.png';
+import gefodi from '../image/gefodi.png';
 
 
 function Projects() {
@@ -25,12 +35,26 @@ function Projects() {
 						>
 							<div className='overlay2 d-flex flex-column justify-content-center align-items-center'>
 								<h4>OhMyCat</h4>
-								<img
-									src={logoImage}
-									alt=''
-									className='imgLogo'
-								/>
-								<a href='#' className='btn'>
+
+								<div className='d-flex gap-2'>
+									<img
+										src={logoJs}
+										alt=''
+										className='imgLogo'
+									/>
+									<img
+										src={logoCss}
+										alt=''
+										className='imgLogo'
+									/>
+									<img
+										src={logoHtml}
+										alt=''
+										className='imgLogo'
+									/>
+								</div>
+
+								<a href='#' className='btn mt-3'>
 									Voir sur GitHub
 								</a>
 							</div>
@@ -48,8 +72,105 @@ function Projects() {
 						>
 							<div className='overlay2 d-flex flex-column justify-content-center align-items-center'>
 								<h4>Biblionest</h4>
+
+								<div className='d-flex gap-2'>
+									<img
+										src={logoLaravel}
+										alt=''
+										className='imgLogo'
+									/>
+									<img
+										src={logoPhp}
+										alt=''
+										className='imgLogo'
+									/>
+								</div>
+
+								<a href='#' className='btn mt-3'>
+									Voir sur GitHub
+								</a>
+							</div>
+						</div>
+					</div>
+					<div className='col-md-4 mb-3'>
+						<div
+							className='card position-relative project-card text-white'
+							style={{
+								height: '165px',
+								backgroundImage: `url(${todolist})`,
+								backgroundSize: 'cover',
+								backgroundPosition: 'center',
+							}}
+						>
+							<div className='overlay2 d-flex flex-column justify-content-center align-items-center'>
+								<h4>OhMyCat</h4>
+
+								<div className='d-flex gap-2'>
+									<img
+										src={logoJava}
+										alt=''
+										className='imgLogo'
+									/>
+									<img
+										src={logoJavafx}
+										alt=''
+										className='imgLogo'
+									/>
+								</div>
+
+								<a href='#' className='btn mt-3'>
+									Voir sur GitHub
+								</a>
+							</div>
+						</div>
+					</div>
+					<div className='col-md-4 mb-3'>
+						<div
+							className='card position-relative project-card text-white'
+							style={{
+								height: '165px',
+								backgroundImage: `url(${gefodi})`,
+								backgroundSize: 'cover',
+								backgroundPosition: 'center',
+							}}
+						>
+							<div className='overlay2 d-flex flex-column justify-content-center align-items-center'>
+								<h4>OhMyCat</h4>
+
+								<div className='d-flex gap-2'>
+									<img
+										src={logoWordpress}
+										alt=''
+										className='imgLogo'
+									/>
+									<img
+										src={logoElementor}
+										alt=''
+										className='imgLogo'
+									/>
+								</div>
+
+								<a href='#' className='btn mt-3'>
+									Voir sur GitHub
+								</a>
+							</div>
+						</div>
+					</div>
+
+					<div className='col-md-4 mb-3 animate'>
+						<div
+							className='card h-200 position-relative project-card'
+							style={{ height: '170px' }}
+						>
+							<div className='card-body'>
+								<h5 className='card-title'>Nom du projet</h5>
+								<p className='card-text'>
+									Petite description du projet ici.
+								</p>
+							</div>
+							<div className='overlay2 d-flex flex-column justify-content-center align-items-center'>
 								<img
-									src={logoImage}
+									src={logoLaravel}
 									alt=''
 									className='imgLogo'
 								/>
@@ -72,76 +193,7 @@ function Projects() {
 							</div>
 							<div className='overlay2 d-flex flex-column justify-content-center align-items-center'>
 								<img
-									src={logoImage}
-									alt=''
-									className='imgLogo'
-								/>
-								<a href='#' className='btn'>
-									Voir sur GitHub
-								</a>
-							</div>
-						</div>
-					</div>
-					<div className='col-md-4 mb-3 animate'>
-						<div
-							className='card h-200 position-relative project-card'
-							style={{ height: '170px' }}
-						>
-							<div className='card-body'>
-								<h5 className='card-title'>Nom du projet</h5>
-								<p className='card-text'>
-									Petite description du projet ici.
-								</p>
-							</div>
-							<div className='overlay2 d-flex flex-column justify-content-center align-items-center'>
-								<img
-									src={logoImage}
-									alt=''
-									className='imgLogo'
-								/>
-								<a href='#' className='btn'>
-									Voir sur GitHub
-								</a>
-							</div>
-						</div>
-					</div>
-					<div className='col-md-4 mb-3 animate'>
-						<div
-							className='card h-200 position-relative project-card'
-							style={{ height: '170px' }}
-						>
-							<div className='card-body'>
-								<h5 className='card-title'>Nom du projet</h5>
-								<p className='card-text'>
-									Petite description du projet ici.
-								</p>
-							</div>
-							<div className='overlay2 d-flex flex-column justify-content-center align-items-center'>
-								<img
-									src={logoImage}
-									alt=''
-									className='imgLogo'
-								/>
-								<a href='#' className='btn'>
-									Voir sur GitHub
-								</a>
-							</div>
-						</div>
-					</div>
-					<div className='col-md-4 mb-3 animate'>
-						<div
-							className='card h-200 position-relative project-card'
-							style={{ height: '170px' }}
-						>
-							<div className='card-body'>
-								<h5 className='card-title'>Nom du projet</h5>
-								<p className='card-text'>
-									Petite description du projet ici.
-								</p>
-							</div>
-							<div className='overlay2 d-flex flex-column justify-content-center align-items-center'>
-								<img
-									src={logoImage}
+									src={logoLaravel}
 									alt=''
 									className='imgLogo'
 								/>
